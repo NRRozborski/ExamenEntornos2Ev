984f64a15b6da4ceac493c26ab2cfcb623707689
NRRozborski <norbert.rozborski@educa.madrid.org>
2023-03-01 09:27:45 +0100

    segundo commit

--- esPrimo.java
+++ esPrimo.java
@@ -1,6 +1,9 @@
 public class esPrimo{
     public static boolean esPrio(int n){
         int i= 2;
+        if(n<1){
+            return false;
+        }
         while(i<n){
             if(n%i==0)
                 return false;
