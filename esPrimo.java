7473636ef866ad9cce6c39ab1f43e7273a353fd2
NRRozborski <norbert.rozborski@educa.madrid.org>
2023-03-01 09:20:29 +0100

    primer commit

---
+++ esPrimo.java
@@ -0,0 +1,11 @@
+public class esPrimo{
+    public static boolean esPrio(int n){
+        int i= 2;
+        while(i<n){
+            if(n%i==0)
+                return false;
+            i++;
+        }
+        return true;
+    }
+}
